c169acd916bd8e5b98e3da89e229d6643e7412c2
Michael Franz <michael.franz@hs-augsburg.de>
2025-02-18 08:24:16 +0100

    Half finished RoofSlope Function

---
+++ RevitCmd/RevitCmd/CreateRooffromIfcCmd.cs
@@ -0,0 +1,101 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitCmd.Models;
+using Xbim.Common.Geometry;
+using Xbim.Ifc2x3.GeometricModelResource;
+using Xbim.Ifc2x3.Kernel;
+using Xbim.Ifc2x3.ProductExtension;
+using Xbim.Ifc2x3.ProfileResource;
+using Xbim.Ifc2x3.RepresentationResource;
+using Xbim.Ifc2x3.SharedBldgElements;
+
+namespace RevitCmd
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CreateRooffromIfcCmd : IExternalCommand
+    {
+        public static UIApplication Application { get; private set; }
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiDoc = commandData.Application.ActiveUIDocument;
+            Application = commandData.Application;
+            string fullPath = "";
+
+            try
+            {
+                RunProgram(uiDoc.Document, fullPath);
+            }
+            catch (Exception e)
+            {
+                TaskDialog.Show(e.GetType().Name, $"{e.Message}{Environment.NewLine}{e.StackTrace}");
+            }
+
+            return Result.Succeeded;
+        }
+        public static void RunProgram(Document document, string fullPath)
+        {
+            var allRoofSlopes = CalculateRoofSlopes.Run(fullPath);
+
+            // generate Roofs from the calculated Slopes
+            foreach (KeyValuePair<IfcRoof, List<XbimVector3D>> entry in allRoofSlopes)
+            {
+                var slab1 = entry.Key.IsDecomposedBy as IfcRelAggregates;
+                var slab2 = slab1.RelatedObjects.First() as IfcElementAssembly;
+                var slab3 = slab2.IsDecomposedBy.First() as IfcRelAggregates;
+                var slab = slab3.RelatedObjects.First() as IfcSlab;
+                var slabRepresentation = slab.Representation as IfcProductDefinitionShape;
+                var representationRepresentations = slabRepresentation.Representations.First() as IfcShapeRepresentation;
+                var items = representationRepresentations.Items.First() as IfcExtrudedAreaSolid;
+                var sweptArea = items.SweptArea as IfcRectangleProfileDef;
+                double xDim = sweptArea.XDim;
+                double yDim = entry.Value[0].Y;
+                var profile = new CurveArray();
+
+                var profileLines = new List<XYZ>()
+                {
+                    XYZ.Zero,
+                    new XYZ(xDim, 0, 0),
+                    new XYZ(xDim, yDim, 0),
+                    new XYZ(0, yDim, 0)
+                }.ToLines(closed: true);
+                foreach (var curve in profileLines)
+                {
+                    profile.Append(curve);
+                }
+                FootPrintRoof roof = document.Create.NewFootPrintRoof(profile, level, type,
+                out ModelCurveArray modelCurves);
+                foreach (var line in modelCurves.QuCurves<ModelLine>())
+                {
+                    var test = line.Location as LocationCurve;
+                    test.Curve
+
+
+
+                    roof.SetSlope(line, 0.5);
+                }
+
+                foreach (XbimVector3D vector in entry.Value)
+                {
+                    roof.SetSlope(line, 0.5);
+                }
+                roof.EaveCuts = EaveCutterType.TwoCutSquare; // What is this??
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
--- RevitCmd/RevitCmd/MyProgram.cs
+++ RevitCmd/RevitCmd/MyProgram.cs
@@ -13,68 +13,10 @@ namespace RevitCmd
 
             var model = new Model(new List<IElement>
             {
-                // todo
-                // new WallElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new Dimension{ X= 10000, Y = 500, Z = 3000 }),
-                // new WallElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = -1, Y =0, Z= 0 }, new Dimension{ X= 500, Y = 5000, Z = 3000 }),
-                // new RoofElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new Dimension{ X= 10000, Y = 5000, Z = 2000 })
+                new WallElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new Dimension{ X= 10000, Y = 500, Z = 3000 }),
+                new WallElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = -1, Y =0, Z= 0 }, new Dimension{ X= 500, Y = 5000, Z = 3000 }),
+                new RoofElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new Dimension{ X= 10000, Y = 5000, Z = 2000 })
             });
-
-            //var json = model.SerializeJson();
-
-            //var file = new FileInfo(_docPath + "/JSON/JsonTest.json");
-            //if (!file.Directory.Exists)
-            //{
-            //    Directory.CreateDirectory(file.DirectoryName);
-            //}
-            //File.WriteAllText(file.FullName, json);
-            //var modelFromjson = json.DeserializeJson<Model>();
-
-            //document.Transaction(_ =>
-            //{
-            //    model.Draw(document);
-            //});
-
-            //var json = model.SerializeJson();
-
-            //var model2 = json.DeserializeJson<ModelDtO>();
-
-            //document.Transaction(_ =>
-            //{
-            //    var walls = document.QuOfType<Wall>();
-            //    foreach (var wall in walls)
-            //    {
-            //        var geo = wall.get_Geometry(
-            //            new Options()
-            //            {
-            //                View = document.ActiveView,
-            //                ComputeReferences = true, // this is important since we need references for measuring
-            //                IncludeNonVisibleObjects = false
-            //            });
-            //        var solids = geo.OfType<Solid>();
-            //        foreach (var solid in solids)
-            //        {
-            //            var bottomFace = solid.Faces.OfType<Face>()
-            //                            .Where(o => o.ComputeNormal(new UV(.5, .5)).Z < 0).First(); // get bottom face
-            //            var ordered = bottomFace
-            //                .EdgeLoops.OfType<EdgeArray>()
-            //                .SelectMany(o => o.OfType<Edge>())
-            //                .OrderBy(o => o.ApproximateLength);
-
-            //            // first 2 smallest opposit
-            //            // last 2 longest opposit
-            //            var edge0 = ordered.ElementAt(0);
-            //            var edge1 = ordered.ElementAt(1);
-            //            var line = ordered.ElementAt(3).AsCurve() as Line;
-            //            double offset = 2000;
-
-            //            document.CreateDimension(edge0, edge1, line, offset);
-            //        }
-            //    }
-
-            //});
-            //todo: attach walls to roof!
-            // Done!
-
 
         }
 
