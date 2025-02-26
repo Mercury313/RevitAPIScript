73d8d09ddfdf58c2c9147800f96c2b17e1b722df
SSeelos <sebastian.seelos@gmx.de>
2025-02-26 14:30:59 +0100

    use roofshape

--- RevitCmd/RevitCmd/MyProgram.cs
+++ RevitCmd/RevitCmd/MyProgram.cs
@@ -10,14 +10,12 @@ namespace RevitCmd
             //var json = System.IO.File.ReadAllText(_docPath + "/model.json");
 
             //var model = JsonConvert.DeserializeObject<IModel>(json);
-
-
             var model = new Model(new List<IElement>
                 {
                     //todo
                     new WallElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new Dimension{ X= 10000, Y = 500, Z = 3000 }),
                     // new WallElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = -1, Y =0, Z= 0 }, new Dimension{ X= 500, Y = 5000, Z = 3000 }),
-                    //new RoofElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new Dimension{ X= 10000, Y = 5000, Z = 2000 })
+                    new RoofElement(new Vector{ X= 0, Y=0,Z=0 }, new Vector{X = 1, Y =0, Z= 0 }, new RoofShape{ X= 10000, Y = 5000, Z = 2000, AngleA = 30, AngleB = 20 })
                 });
 
             document.Transaction(_ =>
