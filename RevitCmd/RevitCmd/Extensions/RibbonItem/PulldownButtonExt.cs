0b93aeb4278e4ba2b7ad8ec0afc71ec95e0684c3
SSeelos <sebastian.seelos@gmx.de>
2025-02-26 15:29:20 +0100

    added external app + addin file added addin button tp ribbon panel

---
+++ RevitCmd/RevitCmd/Extensions/RibbonItem/PulldownButtonExt.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.UI;
+
+namespace RevitCmd
+{
+    public static class PulldownButtonExt
+    {
+        public static PushButton AddPushButton<T>(this PulldownButton pulldown, string txt = null)
+        {
+            Type type = typeof(T);
+            txt ??= type.Name;
+
+            var data = new PushButtonData(type.Name, txt, type.Assembly.Location, type.FullName);
+            return pulldown.AddPushButton(data);
+        }
+    }
+}
